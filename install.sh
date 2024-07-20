c3234cc3e6429424dc49e7ace5db54121a44a8fe
Pieter van der Heijden <pietervdheijden@gmail.com>
2024-07-20 14:13:02 +0200

    Added confirmation before installing packages to install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,14 @@
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 # Install + configure packages
-$SCRIPT_DIR/pkgs/install.sh
+read -r -p "*** Do you want to install packages? Press y if you do. " -n 1;
+echo ""
+
+if [[ $REPLY =~ ^[y]$ ]]; then
+  $SCRIPT_DIR/pkgs/install.sh
+else
+  echo "** Skipping installation of packages..."
+fi
 
 # Create symlink for dotfiles
 echo "*** Stowing dotfiles..."
