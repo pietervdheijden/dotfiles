6e695c320733762760fd03679f6678c2a5760103
Pieter van der Heijden <pietervdheijden@gmail.com>
2024-07-24 07:36:41 +0200

    Refactored zsh dependencies.sh

--- dotfiles/.config/zsh/dependencies.sh
+++ dotfiles/.config/zsh/dependencies.sh
@@ -12,22 +12,22 @@ complete -o nospace -C /usr/local/bin/terraform terraform
 # Load Kubectl CLI autocompletion
 source <(kubectl completion zsh)
 
-### ZSH
+# Oh My ZSH
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
-
-# Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
-
-# ZSH theme
 ZSH_THEME="powerlevel10k/powerlevel10k"
+source $ZSH/oh-my-zsh.sh
+[[ ! -f ~/.config/zsh/.p10k.zsh ]] || source ~/.config/zsh/.p10k.zsh # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 
+# Zsh configs
 zstyle ':omz:update' mode auto      # update automatically without asking
 
+# Plugins
 plugins=(
     git
     sudo
@@ -37,8 +37,3 @@ plugins=(
     zsh-completions
     kubectl
 )
-
-source $ZSH/oh-my-zsh.sh
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.config/zsh/.p10k.zsh ]] || source ~/.config/zsh/.p10k.zsh
