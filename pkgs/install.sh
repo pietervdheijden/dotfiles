2dcc1341ad3443dc9cf97f4daf3beae2dee920e5
Pieter van der Heijden <pietervdheijden@gmail.com>
2024-07-25 17:18:29 +0200

    Added Google Cloud SDK installation

--- pkgs/install.sh
+++ pkgs/install.sh
@@ -32,6 +32,15 @@ else
 
   sudo apt-get install -y stow
   sudo apt-get install -y zsh
+
+  # Install Google Cloud SDK
+  echo "*** Installing Google Cloud SDK..."
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+  sudo apt-get update
+  sudo apt-get -y install google-cloud-cli
+  sudo apt-get -y install google-cloud-sdk-gke-gcloud-auth-plugin
+
 fi
 
 # NPM
