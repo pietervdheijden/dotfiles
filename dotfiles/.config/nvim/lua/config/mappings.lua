3128b9b31200fb437f00b41f359ab256abc54b50
Pieter van der Heijden <pietervdheijden@gmail.com>
2024-07-29 21:36:46 +0200

    nvim: added mappings for plugin Telescope

--- dotfiles/.config/nvim/lua/config/mappings.lua
+++ dotfiles/.config/nvim/lua/config/mappings.lua
@@ -4,6 +4,13 @@ local map = vim.keymap.set
 
 function This.setup()
     map('n', '<leader>fn', vim.cmd.NvimTreeFindFileToggle, { desc = 'open file tree' })
+
+    -- Telescope
+    local builtin = require('telescope.builtin')
+    map('n', '<leader>ff', builtin.find_files, { desc = "TS: Find files" })
+    map('n', '<leader>fg', builtin.live_grep, { desc = "TS: Live grep" })
+    map('n', '<leader>fb', builtin.buffers, { desc = "TS: Buffers" })
+    map('n', '<leader>fh', builtin.help_tags, {desc = "TS: Help tags" })
 end
 
 return This
