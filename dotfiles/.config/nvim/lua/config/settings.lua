cd6438c689ee37abce73c68d6016ccd9da84ef22
Pieter van der Heijden <pietervdheijden@gmail.com>
2024-07-30 06:24:29 +0200

    nvim: configure tabs

--- dotfiles/.config/nvim/lua/config/settings.lua
+++ dotfiles/.config/nvim/lua/config/settings.lua
@@ -13,8 +13,11 @@ function This.setup()
     vim.g.mapleader = " "
     vim.g.maplocalleader = "\\"
 
+    -- Configure tabs
     vim.opt.expandtab = true
     vim.opt.tabstop = 2
+    vim.opt.softtabstop = 2
+    vim.opt.shiftwidth = 2
 end
 
 return This
