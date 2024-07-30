3a5a372689318ca41ae5a5691dbbf2b16a6a8591
Pieter van der Heijden <pietervdheijden@gmail.com>
2024-07-30 06:43:54 +0200

    nvim: include hidden files in telescope commands

--- dotfiles/.config/nvim/lua/plugins/telescope.lua
+++ dotfiles/.config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,27 @@
 return {
     'nvim-telescope/telescope.nvim', 
     tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 
+        'nvim-lua/plenary.nvim',
+        -- "nvim-telescope/telescope-fzf-native.nvim",
+        -- "nvim-telescope/telescope-smart-history.nvim",
+        -- "nvim-telescope/telescope-ui-select.nvim",
+    },
+    config = function()
+        local telescope = require('telescope')
+        telescope.setup({
+            defaults = {
+                vimgrep_arguments = {
+                    'rg', '--color=never', '--no-heading', '--with-filename',
+                    '--line-number', '--column', '--smart-case', '--hidden',
+                    "-g", "!.git"
+                },
+            },
+            pickers = {
+                find_files = {
+                    find_command = {'rg', '--files', '--hidden', '-g', '!.git'},
+                }
+            }
+        }) 
+    end
 }
