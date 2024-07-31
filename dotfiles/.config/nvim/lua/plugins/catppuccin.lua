063081001b56732e327d416bf7e0136fa0f97ec0
Pieter van der Heijden <pietervdheijden@gmail.com>
2024-07-31 20:52:45 +0200

    nvim: installed theme catppuccin.lua

---
+++ dotfiles/.config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,19 @@
+return { 
+    "catppuccin/nvim", 
+    name = "catppuccin", 
+    priority = 1000,
+    opts = {
+      integrations = {
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        telescope = {
+          enabled = true
+        },
+        which_key = true
+      }
+    },
+    config = function () 
+      vim.cmd.colorscheme "catppuccin"
+    end
+}
