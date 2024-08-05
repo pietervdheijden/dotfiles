4dbb31366271a7315d7fd69a6e0ca9859f8740fa
Pieter van der Heijden <pietervdheijden@gmail.com>
2024-08-05 20:44:43 +0200

    nvim: added WSL clipboard configuration

--- dotfiles/.config/nvim/lua/config/settings.lua
+++ dotfiles/.config/nvim/lua/config/settings.lua
@@ -1,23 +1,39 @@
 local This = {}
 
 function This.setup()
-    -- Disable netrw to prevent race conditions at startup with nvimtree 
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+  -- Disable netrw to prevent race conditions at startup with nvimtree 
+  vim.g.loaded_netrw = 1
+  vim.g.loaded_netrwPlugin = 1
 
-    -- Disable optional providers
-    vim.g.loaded_perl_provider = 0
-    vim.g.loaded_python3_provider = 0
-    vim.g.loaded_ruby_provider = 0
+  -- Disable optional providers
+  vim.g.loaded_perl_provider = 0
+  vim.g.loaded_python3_provider = 0
+  vim.g.loaded_ruby_provider = 0
 
-    vim.g.mapleader = " "
-    vim.g.maplocalleader = "\\"
+  vim.g.mapleader = " "
+  vim.g.maplocalleader = "\\"
 
-    -- Configure tabs
-    vim.opt.expandtab = true
-    vim.opt.tabstop = 2
-    vim.opt.softtabstop = 2
-    vim.opt.shiftwidth = 2
+  -- Configure tabs
+  vim.opt.expandtab = true
+  vim.opt.tabstop = 2
+  vim.opt.softtabstop = 2
+  vim.opt.shiftwidth = 2
+
+  -- Configure clipboard
+  if vim.fn.has('wsl') == 1 then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = 'clip.exe',
+            ['*'] = 'clip.exe',
+        },
+        paste = {
+            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = 0,
+    }
+  end
 end
 
 return This
