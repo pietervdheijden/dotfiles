4544c27bc924a68a0da9d81a336e82748c955763
Pieter van der Heijden <pietervdheijden@gmail.com>
2024-08-20 07:46:52 +0200

    Nvim: reformatted mappings.lua

--- dotfiles/.config/nvim/lua/config/mappings.lua
+++ dotfiles/.config/nvim/lua/config/mappings.lua
@@ -3,14 +3,15 @@ local This = {}
 local map = vim.keymap.set
 
 function This.setup()
-    map('n', '<leader>fn', vim.cmd.NvimTreeFindFileToggle, { desc = 'open file tree' })
+  -- Nvim-tree  
+  map('n', '<leader>fn', vim.cmd.NvimTreeFindFileToggle, { desc = 'open file tree' })
 
-    -- Telescope
-    local builtin = require('telescope.builtin')
-    map('n', '<leader>ff', builtin.find_files, { desc = "TS: Find files" })
-    map('n', '<leader>fg', builtin.live_grep, { desc = "TS: Live grep" })
-    map('n', '<leader>fb', builtin.buffers, { desc = "TS: Buffers" })
-    map('n', '<leader>fh', builtin.help_tags, {desc = "TS: Help tags" })
+  -- Telescope
+  local telescope = require('telescope.builtin')
+  map('n', '<leader>ff', telescope.find_files, { desc = "TS: Find files" })
+  map('n', '<leader>fg', telescope.live_grep, { desc = "TS: Live grep" })
+  map('n', '<leader>fb', telescope.buffers, { desc = "TS: Buffers" })
+  map('n', '<leader>fh', telescope.help_tags, {desc = "TS: Help tags" })
 end
 
 return This
