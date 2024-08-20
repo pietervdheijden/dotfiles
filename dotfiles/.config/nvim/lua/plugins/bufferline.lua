27acffad7f433c6a68ec0709676613b21a8294f1
Pieter van der Heijden <pietervdheijden@gmail.com>
2024-08-20 07:48:41 +0200

    Nvim: add keymap to delete buffer

--- dotfiles/.config/nvim/lua/plugins/bufferline.lua
+++ dotfiles/.config/nvim/lua/plugins/bufferline.lua
@@ -4,11 +4,11 @@ return {
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function () 
     require("bufferline").setup{
-			options = {
-				indicator = {
-					style = "underline",
-				},
-				separator_style = "slant",
+      options = {
+        indicator = {
+          style = "underline",
+        },
+        separator_style = "slant",
         themeable = true,
         show_close_icon = false,
         hover = {
@@ -25,7 +25,25 @@ return {
               separator = true
           }
         },
-			},
+        always_show_bufferline = true,
+      },
     }
+
+    -- Custom function to delete buffer and avoid focusing on Nvim Tree
+    _G.delete_buffer = function()
+      local bufnr = vim.fn.bufnr()
+      -- Cycle to the next buffer
+      vim.cmd('BufferLineCycleNext')
+      -- Check if the new buffer is Nvim Tree
+      if vim.bo.filetype == "NvimTree" then
+        -- If it is, cycle again to avoid Nvim Tree
+        vim.cmd('BufferLineCycleNext')
+      end
+      -- Finally, delete the original buffer
+      vim.cmd('bdelete! '..bufnr)
+    end
+
+    -- Key mappings for deleting buffers
+    vim.keymap.set('n', '<leader>bd', ':lua delete_buffer()<CR>', { noremap = true, silent = true, desc = "Delete buffer" })
   end,
 }
