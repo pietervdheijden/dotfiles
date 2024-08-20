a5426c76b3b1fb26ca83207b065d1898347396f3
Pieter van der Heijden <pietervdheijden@gmail.com>
2024-08-20 07:20:41 +0200

    Nvim: update config nvim-tree
    - Open nvim-tree upon VimEnter
    - Track active file

--- dotfiles/.config/nvim/lua/plugins/nvim-tree.lua
+++ dotfiles/.config/nvim/lua/plugins/nvim-tree.lua
@@ -21,6 +21,25 @@ return {
         },
         git = {
             ignore = false,
-        }
-    }
+        },
+    },
+    config = function()
+        require("nvim-tree").setup(opts)
+
+        -- Configuration for automatically opening nvim-tree
+        vim.api.nvim_create_autocmd("VimEnter", {
+            callback = function()
+                if vim.fn.argc() == 0 then
+                    require("nvim-tree.api").tree.open()
+                end
+            end
+        })
+
+        -- Ensure nvim-tree tracks the open file
+        vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter", "TabEnter"}, {
+            callback = function()
+                require("nvim-tree.api").tree.find_file({ open = true })
+            end,
+        })
+    end
 }
