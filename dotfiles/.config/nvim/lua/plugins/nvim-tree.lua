af1c6623612b1f3f4ab095040d27de086b447ef6
Pieter van der Heijden <pietervdheijden@gmail.com>
2024-08-20 19:30:05 +0200

    Nvim: do not automatically open nvim-tree in kubectl edit

--- dotfiles/.config/nvim/lua/plugins/nvim-tree.lua
+++ dotfiles/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,45 +1,51 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons"
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons"
+  },
+  opts = {
+    sort = {
+      sorter = "case_sensitive",
     },
-    opts = {
-        sort = {
-            sorter = "case_sensitive",
-        },
-        view = {
-            width = 30,
-        },
-        renderer = {
-            group_empty = true,
-        },
-        filters = {
-            dotfiles = false,
-            custom = { "^\\.git$" , "node_modules", ".cache" } 
-        },
-        git = {
-            ignore = false,
-        },
+    view = {
+      width = 30,
     },
-    config = function()
-        require("nvim-tree").setup(opts)
+    renderer = {
+      group_empty = true,
+    },
+    filters = {
+      dotfiles = false,
+      custom = { "^\\.git$" , "node_modules", ".cache" } 
+    },
+    git = {
+      ignore = false,
+    },
+  },
+  config = function()
+    require("nvim-tree").setup(opts)
 
-        -- Configuration for automatically opening nvim-tree
-        vim.api.nvim_create_autocmd("VimEnter", {
-            callback = function()
-                if vim.fn.argc() == 0 then
-                    require("nvim-tree.api").tree.open()
-                end
-            end
-        })
+    -- Helper function to check if Neovim was opened by `kubectl edit`
+    local function is_kubectl_edit()
+      -- Check the command line arguments for "kubectl edit"
+      local arglist = vim.fn.argv()
+      for _, arg in ipairs(arglist) do
+        if arg:match("kubectl") and arg:match("edit") then
+          return true
+        end
+      end
 
-        -- Ensure nvim-tree tracks the open file
-        vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter", "TabEnter"}, {
-            callback = function()
-                require("nvim-tree.api").tree.find_file({ open = true })
-            end,
-        })
+      return false
     end
+
+    -- Ensure nvim-tree tracks the open file
+    vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter", "TabEnter"}, {
+      callback = function()
+        if not is_kubectl_edit() then
+          require("nvim-tree.api").tree.find_file({ open = true })
+        end  
+      end,
+    })
+  end
 }
