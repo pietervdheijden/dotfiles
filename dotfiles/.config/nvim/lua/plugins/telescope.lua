690024148ffd4ff5b5d8ff3a5bb8696e72099186
Pieter van der Heijden <pietervdheijden@gmail.com>
2024-09-03 06:58:35 +0200

    Nvim: use ivy theme for plugin telescope

--- dotfiles/.config/nvim/lua/plugins/telescope.lua
+++ dotfiles/.config/nvim/lua/plugins/telescope.lua
@@ -1,28 +1,46 @@
 return {
-    'nvim-telescope/telescope.nvim', 
-    tag = '0.1.8',
-    dependencies = { 
-        'nvim-lua/plenary.nvim'
-    },
-    config = function()
-        local telescope = require('telescope')
-        telescope.setup({
-            defaults = {
-                vimgrep_arguments = {
-                    'rg', '--color=never', '--no-heading', '--with-filename',
-                    '--line-number', '--column', '--smart-case', '--hidden',
-                    "-g", "!.git"
-                },
-                path_display = function(opts, path)
-                  local tail = require("telescope.utils").path_tail(path)
-                  return string.format("%s - %s", tail, path)
-                end,
-            },
-            pickers = {
-                find_files = {
-                    find_command = {'rg', '--files', '--hidden', '-g', '!.git'},
-                }
-            }
-        }) 
-    end
+  'nvim-telescope/telescope.nvim', 
+  tag = '0.1.8',
+  dependencies = { 
+    'nvim-lua/plenary.nvim'
+  },
+  config = function()
+    local telescope = require('telescope')
+    telescope.setup({
+      defaults = {
+        vimgrep_arguments = {
+          'rg', 
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number', 
+          '--column',
+          '--smart-case',
+          '--hidden',
+          "-g", 
+          "!.git",
+        },
+        path_display = function(opts, path)
+          local tail = require("telescope.utils").path_tail(path)
+          return string.format("%s - %s", tail, path)
+        end,
+      },
+      pickers = {
+        find_files = {
+          find_command = {
+            'rg', 
+            '--files',
+            '--hidden',
+            '-g',
+            '!.git'
+          },
+          theme = 'ivy',
+          previewer = false,
+        },
+        live_grep = {
+            theme = 'ivy',
+        }
+      }
+    }) 
+  end
 }
