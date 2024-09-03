30570c1f115b2a9e509a99f92244e5341aebbb1e
Pieter van der Heijden <pietervdheijden@gmail.com>
2024-09-03 07:06:33 +0200

    Nvim: fix indentation settings.lua

--- dotfiles/.config/nvim/lua/config/settings.lua
+++ dotfiles/.config/nvim/lua/config/settings.lua
@@ -36,16 +36,16 @@ function This.setup()
   -- Configure clipboard
   if vim.fn.has('wsl') == 1 then
     vim.g.clipboard = {
-        name = 'WslClipboard',
-        copy = {
-            ['+'] = 'clip.exe',
-            ['*'] = 'clip.exe',
-        },
-        paste = {
-            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-        },
-        cache_enabled = 0,
+      name = 'WslClipboard',
+      copy = {
+        ['+'] = 'clip.exe',
+        ['*'] = 'clip.exe',
+      },
+      paste = {
+        ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      },
+      cache_enabled = 0,
     }
   end
 end
