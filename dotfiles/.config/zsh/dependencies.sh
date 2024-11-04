efbfcd86e33e810dd7ccdf8b8db5ac229cf51468
pieter-vanderheijden_cmcom <pieter.vanderheijden@cm.com>
2024-11-04 14:31:17 +0100

    zsh: add docker plugin
    This should hopefully fix error:
    
    compinit:503: no such file or directory:
    /usr/share/zsh/vendor-completions/_docker

--- dotfiles/.config/zsh/dependencies.sh
+++ dotfiles/.config/zsh/dependencies.sh
@@ -1,9 +1,22 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+
 if [[ -d /home/linuxbrew/.linuxbrew ]]; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
 # Zsh configs
 zstyle ':omz:update' mode auto      # update automatically without asking
+
+# Oh My ZSH
+export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="powerlevel10k/powerlevel10k"
+source $ZSH/oh-my-zsh.sh
 
 # Plugins
 # Set plugins before loading oh-my-zsh, otherwise plugins will not be used
@@ -16,6 +29,7 @@ plugins=(
     zsh-completions
     kubectl
     helm
+    docker
 )
 
 # command for zsh-completions
@@ -28,17 +42,8 @@ complete -o nospace -C /usr/local/bin/terraform terraform
 # Load Kubectl CLI autocompletion
 source <(kubectl completion zsh)
 
-# Oh My ZSH
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-export ZSH="$HOME/.oh-my-zsh"
-ZSH_THEME="powerlevel10k/powerlevel10k"
-source $ZSH/oh-my-zsh.sh
-[[ ! -f ~/.config/zsh/.p10k.zsh ]] || source ~/.config/zsh/.p10k.zsh # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+# Load p10k configuration
+[[ ! -f ~/.config/zsh/.p10k.zsh ]] || source ~/.config/zsh/.p10k.zsh
 
 # Load NVM configuration
 export NVM_DIR="$HOME/.nvm"
