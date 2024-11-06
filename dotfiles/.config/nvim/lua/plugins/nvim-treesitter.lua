98e9c3d4c191e5063897b97f2b2ceafdb15e4b2a
Pieter van der Heijden <pietervdheijden@gmail.com>
2024-11-06 22:11:36 +0100

    nvim: install terraform in treesitter

--- dotfiles/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,7 @@ return {
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html", "java", "python" },
+            ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html", "java", "python", "terraform" },
             auto_install = true,
             sync_install = false,
             highlight = { enable = true },
