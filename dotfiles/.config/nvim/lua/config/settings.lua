df078af576d572cf2bfb1e943f29e4c27ded5728
Pieter van der Heijden <pietervdheijden@gmail.com>
2024-11-11 16:13:45 +0100

    nvim: copy to system clipboard

--- dotfiles/.config/nvim/lua/config/settings.lua
+++ dotfiles/.config/nvim/lua/config/settings.lua
@@ -34,20 +34,22 @@ function This.setup()
   vim.opt.splitright = true
 
   -- Configure clipboard
-  if vim.fn.has('wsl') == 1 then
-    vim.g.clipboard = {
-      name = 'WslClipboard',
-      copy = {
-        ['+'] = 'clip.exe',
-        ['*'] = 'clip.exe',
-      },
-      paste = {
-        ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-        ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      },
-      cache_enabled = 0,
-    }
-  end
+  -- if vim.fn.has('wsl') == 1 then
+  --   vim.g.clipboard = {
+  --     name = 'WslClipboard',
+  --     copy = {
+  --       ['+'] = 'clip.exe',
+  --       ['*'] = 'clip.exe',
+  --     },
+  --     paste = {
+  --       ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  --       ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  --     },
+  --     cache_enabled = 0,
+  --   }
+  -- end
+  vim.opt.clipboard:append("unnamedplus")
+
 end
 
 return This
