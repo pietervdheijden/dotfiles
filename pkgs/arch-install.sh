01826059ad6b648362812b234730ba82a03c3509
Pieter van der Heijden <pietervanderheijden@CM-4BRP5M3.>
2024-11-20 21:50:21 +0100

    arch: install google-cloud-cli

--- pkgs/arch-install.sh
+++ pkgs/arch-install.sh
@@ -53,6 +53,8 @@ fi
 echo "*** Installing AUR packages with yay..."
 yay --noconfirm --needed -S jdtls # Java Development Tools Language Server  
 yay --noconfirm --needed -S terraform-ls # Terraform Language Server
+yay --noconfirm --needed -S google-cloud-cli
+yay --noconfirm --needed -S google-cloud-cli-gke-gcloud-auth-plugin
 
 # Install wslu on WSL
 if [[ ! -x "$(command -v wslview)" ]] && grep -qi microsoft /proc/version; then
