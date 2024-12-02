250eb5c45833b9b94909d0feffc8591088bc5256
Pieter van der Heijden <pietervdheijden@gmail.com>
2024-12-02 06:51:32 +0100

    nvim: merge jdtls config files

--- dotfiles/.config/nvim/after/ftplugin/java.lua
+++ dotfiles/.config/nvim/after/ftplugin/java.lua
@@ -7,6 +7,18 @@ vim.opt_local.shiftwidth = 4
 
 local jdtls = require('jdtls')
 
+    -- local install_path = require("mason-registry").get_package("jdtls"):get_install_path()
+    -- -- get the debug adapter install path
+    -- local debug_install_path = require("mason-registry").get_package("java-debug-adapter"):get_install_path()
+    -- local bundles = {
+    --   vim.fn.glob(debug_install_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar", 1),
+    -- }
+
+    -- Comment out these lines if you have 'java-test' installed
+    -- local java_test_path = require("mason-registry").get_package("java-test"):get_install_path()
+    -- vim.list_extend(bundles, vim.split(vim.fn.glob(java_test_path .. "/extension/server/*.jar", 1), "\n"))
+
+
 local home = vim.fn.expand("~")
 local mason_path = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
 -- local lombok_path = home .. "/.config/nvim/lombok/lombok.jar"
@@ -15,7 +27,7 @@ local lombok_path = mason_path .. "/lombok.jar"
 -- Define workspace path
 local workspace_path = home .. "/.local/share/nvim/java_workspace/"
 
-print(lombok_path)
+-- print(lombok_path)
 
 -- Configure `nvim-jdtls`
 local config = {
@@ -29,17 +41,75 @@ local config = {
     '-Dlog.level=ALL',
     '-Xms1g',
     '-jar', vim.fn.glob(mason_path .. '/plugins/org.eclipse.equinox.launcher_*.jar'),
-    '-configuration', mason_path .. '/config_linux', -- Adjust `config_linux` for your OS
+    '-configuration', mason_path .. '/config_linux',
     '-data', workspace_path .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t"),
+        -- "--add-modules=ALL-SYSTEM",
+        -- "--add-opens",
+        -- "java.base/java.util=ALL-UNNAMED",
+        -- "--add-opens",
+        -- "java.base/java.lang=ALL-UNNAMED",
   },
-  root_dir = require('jdtls.setup').find_root({ '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' }),
+    -- local on_attach = function(client, bufnr)
+    --   require("plugins.lspconfig").on_attach(client, bufnr)
+    -- end
+    --
+    -- local capabilities = require("plugins.lspconfig").capabilities
+  -- on_attach = on_attach,
+  -- capabilities = capabilities,
+  root_dir = require('jdtls.setup').find_root({ '.git' }),
   settings = {
+  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+  -- for a list of options
     java = {
       format = {
         enabled = true,
+        settings = {
+          -- Use Google Java style guidelines for formatting
+          -- To use, make sure to download the file from https://github.com/google/styleguide/blob/gh-pages/eclipse-java-google-style.xml
+          -- and place it in the ~/.local/share/eclipse directory
+          url = "/.local/share/eclipse/eclipse-java-google-style.xml",
+          profile = "GoogleStyle",
+        },
       },
       saveActions = {
         organizeImports = true,
+      },
+      signatureHelp = { enabled = true },
+      contentProvider = { preferred = 'fernflower' },  -- Use fernflower to decompile library code
+      -- Specify any completion options
+      completion = {
+        favoriteStaticMembers = {
+          "org.hamcrest.MatcherAssert.assertThat",
+          "org.hamcrest.Matchers.*",
+          "org.hamcrest.CoreMatchers.*",
+          "org.junit.jupiter.api.Assertions.*",
+          "java.util.Objects.requireNonNull",
+          "java.util.Objects.requireNonNullElse",
+          "org.mockito.Mockito.*"
+        },
+        filteredTypes = {
+          "com.sun.*",
+          "io.micrometer.shaded.*",
+          "java.awt.*",
+          "jdk.*", "sun.*",
+        },
+      },
+      -- Specify any options for organizing imports
+      sources = {
+        organizeImports = {
+          starThreshold = 9999;
+          staticStarThreshold = 9999;
+        },
+      },
+      -- How code generation should act
+      codeGeneration = {
+        toString = {
+          template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
+        },
+        hashCodeEquals = {
+          useJava7Objects = true,
+        },
+        useBlocks = true,
       },
     },
   },
@@ -54,3 +124,9 @@ local config = {
 -- Start or attach `nvim-jdtls`
 jdtls.start_or_attach(config)
 
+    -- vim.api.nvim_create_autocmd("FileType", {
+    --   pattern = "java",
+    --   callback = function()
+    --     require("jdtls").start_or_attach(config)
+    --   end,
+    -- })
--- dotfiles/.config/nvim/lua/plugins/lspconfig.lua
+++ dotfiles/.config/nvim/lua/plugins/lspconfig.lua
@@ -24,35 +24,47 @@ return {
         -- Default handler for all installed servers
         function(server_name)
           if server_name == 'jdtls' then
-              -- These require specialized setup
-              return
+            -- These require specialized setup
+            return
           end
           require('lspconfig')[server_name].setup({
             on_attach = function(client, bufnr)
               -- Enable completion triggered by <c-x><c-o>
               vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
+              -- Helper function for creating keymaps
+              function nnoremap(rhs, lhs, bufopts, desc)
+                bufopts.desc = desc
+                vim.keymap.set("n", rhs, lhs, bufopts)
+              end
+
               -- Mappings.
-              local opts = { noremap=true, silent=true }
+              local function opts(desc)
+                return { desc = 'LSP: ' .. desc, noremap = true, silent = true }
+              end
               -- See `:help vim.lsp.*` for documentation on any of the below functions
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+              nnoremap('gD', vim.lsp.buf.declaration, bufopts, 'Go to declaration')
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts('Go to definition'))
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts('Go to implementation'))
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts('Hover text'))
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts('Show signature'))
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts('Add workspace folder'))
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts('Remove workspace folder'))
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts('List workspace folders'))
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts('Go to type definition'))
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts('Rename'))
+              vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts('Find refenerences'))
+              nnoremap('<space>ca', vim.lsp.buf.code_action, bufopts, "Code actions")
+              vim.keymap.set('v', "<space>ca", "<ESC><CMD>lua vim.lsp.buf.range_code_action()<CR>",
+                 { noremap=true, silent=true, buffer=bufnr, desc = "Code actions" })
               -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
               vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
               vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
               vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-              -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>', opts)
+              -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>', opts('Format file'))
 
               -- Reload diagnostics
-              vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rd', '<cmd>lua vim.diagnostic.reset() vim.diagnostic.show()<CR>', opts)
+              -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rd', '<cmd>lua vim.diagnostic.reset() vim.diagnostic.show()<CR>', opts)
             end,
             flags = {
               debounce_text_changes = 150,
@@ -65,6 +77,16 @@ return {
         --     -- Add your jdtls-specific settings here
         --   })
         -- end,
+  
+        -- Java extensions provided by jdtls
+        -- nnoremap("<C-o>", jdtls.organize_imports, bufopts, "Organize imports")
+        -- nnoremap("<space>ev", jdtls.extract_variable, bufopts, "Extract variable")
+        -- nnoremap("<space>ec", jdtls.extract_constant, bufopts, "Extract constant")
+        -- vim.keymap.set('v', "<space>em", [[<ESC><CMD>lua require('jdtls').extract_method(true)<CR>]],
+        --   { noremap=true, silent=true, buffer=bufnr, desc = "Extract method" })
+        --     }
+    -- })
+
       }
     })
 
@@ -76,57 +98,4 @@ return {
       end,
     })
   end
-  --   local lombok_jar = os.getenv('HOME') .. '/.local/share/eclipse/lombok.jar'
-  --   -- require('java').setup({
-  --   --   javaAgent = lombok_jar,  -- Add Lombok agent
-  --   --   jvmArgs = {
-  --   --     '-Xms1g',
-  --   --     '-Xmx2g',
-  --   --     '-javaagent:' .. lombok_jar,
-  --   --   },  
-  --   -- })
-  --   -- local home = os.getenv('HOME')
-  --   -- local lombok_jar = home .. '/.local/share/eclipse/lombok.jar'  -- Update this path
-  --   --
-  --   -- -- Configure jdtls for Java
-  --   -- nvim_lsp.jdtls.setup{
-  --   --     cmd = { 
-  --   --       'jdtls',
-  --   --       '-javaagent:' .. lombok_jar,
-  --   --     },
-  --   --     init_options = {
-  --   --         bundles = {
-  --   --             lombok_jar
-  --   --         },
-  --   --         vmargs = { '-javaagent:' .. lombok_jar },
-  --   --
-  --   --     },
-  --   --     settings = {
-  --   --         java = {
-  --   --             home = '/usr/lib/jvm/java-23-openjdk/bin/java',
-  --   --             -- home = '/home/linuxbrew/.linuxbrew/opt/openjdk@17/libexec',  -- Optional: specify your Java home
-  --   --             use_lombok_agent = true,
-  --   --             configuration = {
-  --   --                 -- Specify the Lombok JAR in the Java runtime options
-  --   --                 runtimes = {
-  --   --                     -- {
-  --   --                     --     name = 'JavaSE-1.8',
-  --   --                     --     path = '/path/to/your/jdk1.8',
-  --   --                     --     vmargs = { '-javaagent:' .. lombok_jar }
-  --   --                     -- },
-  --   --                     -- {
-  --   --                     --     name = 'JavaSE-17',
-  --   --                     --     path = '/home/linuxbrew/.linuxbrew/opt/openjdk@17/libexec',
-  --   --                     --     vmargs = { '-javaagent:' .. lombok_jar }
-  --   --                     -- }
-  --   --                     {
-  --   --                         name = 'JavaSE-23',
-  --   --                         path = '/usr/lib/jvm/java-23-openjdk',
-  --   --                         vmargs = { '-javaagent:' .. lombok_jar }
-  --   --                     }
-  --   --                 }
-  --   --             }
-  --   --         }
-  --   --     },
-  --   -- }
 }
