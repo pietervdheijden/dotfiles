e48b88f52a70df794c8ab2b6e0e324e51ec1c913
Pieter van der Heijden <pieter.vanderheijden@cm.com>
2024-12-13 14:17:08 +0100

    nvim: remove extendedCapabilities from jdtls config
    The extendedCapabilities broke vim.lsp.buf.definition() for external
    packages

--- dotfiles/.config/nvim/after/ftplugin/java.lua
+++ dotfiles/.config/nvim/after/ftplugin/java.lua
@@ -7,27 +7,22 @@ vim.opt_local.shiftwidth = 4
 
 local jdtls = require('jdtls')
 
-    -- local install_path = require("mason-registry").get_package("jdtls"):get_install_path()
-    -- -- get the debug adapter install path
-    -- local debug_install_path = require("mason-registry").get_package("java-debug-adapter"):get_install_path()
-    -- local bundles = {
-    --   vim.fn.glob(debug_install_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar", 1),
-    -- }
+local install_path = require("mason-registry").get_package("jdtls"):get_install_path()
+-- get the debug adapter install path
+local debug_install_path = require("mason-registry").get_package("java-debug-adapter"):get_install_path()
+local bundles = {
+  vim.fn.glob(debug_install_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar", 1),
+}
 
-    -- Comment out these lines if you have 'java-test' installed
-    -- local java_test_path = require("mason-registry").get_package("java-test"):get_install_path()
-    -- vim.list_extend(bundles, vim.split(vim.fn.glob(java_test_path .. "/extension/server/*.jar", 1), "\n"))
+-- Comment out these lines if you have 'java-test' installed
+-- local java_test_path = require("mason-registry").get_package("java-test"):get_install_path()
+-- vim.list_extend(bundles, vim.split(vim.fn.glob(java_test_path .. "/extension/server/*.jar", 1), "\n"))
 
 
 local home = vim.fn.expand("~")
-local mason_path = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
--- local lombok_path = home .. "/.config/nvim/lombok/lombok.jar"
+local mason_path = install_path
 local lombok_path = mason_path .. "/lombok.jar"
-
--- Define workspace path
 local workspace_path = home .. "/.local/share/nvim/java_workspace/"
-
--- print(lombok_path)
 
 -- Configure `nvim-jdtls`
 local config = {
@@ -50,11 +45,6 @@ local config = {
     '-jar', vim.fn.glob(mason_path .. '/plugins/org.eclipse.equinox.launcher_*.jar'),
     '-configuration', mason_path .. '/config_linux',
     '-data', workspace_path .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t"),
-        -- "--add-modules=ALL-SYSTEM",
-        -- "--add-opens",
-        -- "java.base/java.util=ALL-UNNAMED",
-        -- "--add-opens",
-        -- "java.base/java.lang=ALL-UNNAMED",
   },
     -- local on_attach = function(client, bufnr)
     --   require("plugins.lspconfig").on_attach(client, bufnr)
@@ -121,14 +111,13 @@ local config = {
       configuration = {
         updateBuildConfiguration = "automatic",
         runtimes = {
-          -- {
-          --   name = "JavaSE-17",
-          --   path = "/usr/lib/jvm/java-17-openjdk",
-          -- },
           {
             name = "JavaSE-11",
-            path = "/usr/lib/jvm/java-11-openjdk/",
-            default = true,
+            path = "/usr/lib/jvm/java-11-openjdk",
+          },
+          {
+            name = "JavaSE-17",
+            path = "/usr/lib/jvm/java-17-openjdk",
           },
         }
       },
@@ -144,15 +133,15 @@ local config = {
       references = {
         includeDecompiledSources = true,
       },
-      -- import = {
-      --   gradle = {
-      --     enabled = true
-      --   },
-      --   maven = {
-      --     enabled = true
-      --   },
-      --   timeout = 180
-      -- }
+      import = {
+        gradle = {
+          enabled = true
+        },
+        maven = {
+          enabled = true
+        },
+        timeout = 180
+      },
       project = {
         referencedLibraries = {},
       }
@@ -162,9 +151,6 @@ local config = {
     bundles = {
       vim.fn.glob(home .. '/.config/nvim/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar'),
       unpack(vim.fn.glob(home .. '/.config/nvim/vscode-java-test/server/*.jar', 1, 1)),
-    },
-    extendedClientCapabilities = {
-        progressReportProvider = true
     },
   },
 -- Add this to ensure proper project configuration
@@ -181,25 +167,14 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 config.capabilities = capabilities
 
-config.handlers = {
-    -- ["language/status"] = function(_, result)
-    --     print(result)
-    -- end,
-    -- ["$/progress"] = function(_, result, ctx)
-    --     local value = result.value
-    --     if not value then return end
-    --     print(value.message)
-    -- end
-}
-
-vim.lsp.set_log_level("debug")
+-- vim.lsp.set_log_level("debug")
 
 -- Start or attach `nvim-jdtls`
 jdtls.start_or_attach(config)
 
-    -- vim.api.nvim_create_autocmd("FileType", {
-    --   pattern = "java",
-    --   callback = function()
-    --     require("jdtls").start_or_attach(config)
-    --   end,
-    -- })
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "java",
+--   callback = function()
+--     require("jdtls").start_or_attach(config)
+--   end,
+-- })
