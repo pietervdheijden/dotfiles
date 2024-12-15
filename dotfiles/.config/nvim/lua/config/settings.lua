700a2bb86a6759619dcabb13f95c3fdb00d87680
Pieter van der Heijden <pietervdheijden@gmail.com>
2024-12-15 21:54:43 +0100

    nvim: remove commented wsl clipboard config

--- dotfiles/.config/nvim/lua/config/settings.lua
+++ dotfiles/.config/nvim/lua/config/settings.lua
@@ -34,20 +34,6 @@ function This.setup()
   vim.opt.splitright = true
 
   -- Configure clipboard
-  -- if vim.fn.has('wsl') == 1 then
-  --   vim.g.clipboard = {
-  --     name = 'WslClipboard',
-  --     copy = {
-  --       ['+'] = 'clip.exe',
-  --       ['*'] = 'clip.exe',
-  --     },
-  --     paste = {
-  --       ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-  --       ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-  --     },
-  --     cache_enabled = 0,
-  --   }
-  -- end
   vim.opt.clipboard:append("unnamedplus")
 
 end
