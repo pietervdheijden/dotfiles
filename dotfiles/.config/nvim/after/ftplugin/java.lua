3a8860f9aca0ce2f7c0a600d17d66e70e2c4dee7
Pieter van der Heijden <pietervdheijden@gmail.com>
2025-02-16 15:13:39 +0100

    nvim: add mapping for jdtls organize imports

--- dotfiles/.config/nvim/after/ftplugin/java.lua
+++ dotfiles/.config/nvim/after/ftplugin/java.lua
@@ -4,3 +4,8 @@ vim.opt_local.tabstop = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.shiftwidth = 4
 
+-- Configure mapping
+local map = vim.keymap.set
+local jdtls = require('jdtls')
+map('n', '<leader>oi', jdtls.organize_imports, { desc = "JDTLS: organize imports" })
+
