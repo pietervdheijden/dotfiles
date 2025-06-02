df7d5995ca573b412734502f24b9fb99f78b8f52
Pieter van der Heijden <pieter.vanderheijden@cm.com>
2025-06-02 06:28:31 +0200

    nvim: install dap

--- dotfiles/.config/nvim/lua/plugins/jdtls.lua
+++ dotfiles/.config/nvim/lua/plugins/jdtls.lua
@@ -9,7 +9,16 @@ local bundles = {
 
 -- Configure java test
 local java_test_path = vim.fn.stdpath('data') .. '/mason/packages/java-test'
-vim.list_extend(bundles, vim.split(vim.fn.glob(java_test_path .. "/extension/server/*.jar", 1), "\n"))
+-- vim.list_extend(bundles, vim.split(vim.fn.glob(java_test_path .. "/extension/server/*.jar", 1), "\n"))
+-- Only include actual OSGi bundles, not fat JARs or agents
+local java_test_bundles = vim.split(vim.fn.glob(java_test_path .. "/extension/server/*.jar", 1), "\n")
+for _, bundle in ipairs(java_test_bundles) do
+  if not vim.endswith(bundle, "com.microsoft.java.test.runner-jar-with-dependencies.jar") and
+     not vim.endswith(bundle, "jacocoagent.jar") then
+    -- print("Insert bundle: " .. bundle)
+    table.insert(bundles, bundle)
+  end
+end
 
 local home = vim.fn.expand("~")
 local mason_path = install_path
@@ -18,9 +27,14 @@ local workspace_path = home .. "/.local/share/nvim/java_workspace/"
 
 -- Configure `nvim-jdtls`
 local function config()
+  -- Set JAVA_HOME environment variable
+  vim.env.JAVA_HOME = '/usr/lib/jvm/java-21-openjdk'
+  vim.env.PATH = vim.env.JAVA_HOME .. '/bin:' .. vim.env.PATH
+  
   return {
     cmd = {
-      '/usr/lib/jvm/java-24-openjdk/bin/java',
+      -- install_path .. '/bin/jdtls',  -- Use Mason's wrapper script
+      '/usr/lib/jvm/java-21-openjdk/bin/java',
       '-javaagent:' .. lombok_path,
       '-Declipse.application=org.eclipse.jdt.ls.core.id1',
       '-Dosgi.bundles.defaultStartLevel=4',
@@ -35,22 +49,19 @@ local function config()
       'java.base/java.util=ALL-UNNAMED',
       '--add-opens',
       'java.base/java.lang=ALL-UNNAMED',
+
+      -- Use unique server instance
+      '-Declipse.jdt.ls.vmargs=-Dfile.encoding=UTF-8',
+      '-Djava.import.generatesMetadataFilesAtProjectRoot=false',
+
       '-jar', vim.fn.glob(mason_path .. '/plugins/org.eclipse.equinox.launcher_*.jar'),
       '-configuration', mason_path .. '/config_linux',
       '-data', workspace_path .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t"),
     },
-      -- local on_attach = function(client, bufnr)
-      --   require("plugins.lspconfig").on_attach(client, bufnr)
-      -- end
-      --
-      -- local capabilities = require("plugins.lspconfig").capabilities
-    -- on_attach = on_attach,
-    -- capabilities = capabilities,
     root_dir = require('jdtls.setup').find_root({ '.git' }),
     settings = {
-    -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-    -- for a list of options
       java = {
+        home = "/usr/lib/jvm/java-21-openjdk",
         format = {
           enabled = true,
           -- settings = {
@@ -113,6 +124,11 @@ local function config()
               path = "/usr/lib/jvm/java-17-openjdk",
             },
             {
+              name = "JavaSE-21",
+              path = "/usr/lib/jvm/java-21-openjdk",
+              default = true,
+            },
+            {
               name = "JavaSE-24",
               path = "/usr/lib/jvm/java-24-openjdk",
             }
@@ -145,33 +161,95 @@ local function config()
       },
     },
     init_options = {
-      bundles = {
-        vim.fn.glob(home .. '/.config/nvim/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar'),
-        unpack(vim.fn.glob(home .. '/.config/nvim/vscode-java-test/server/*.jar', 1, 1)),
-      },
+      bundles = bundles
     },
-  -- Add this to ensure proper project configuration
-      on_init = function(client, _)
-          client.notify('workspace/didChangeConfiguration', { settings = client.config.settings })
-      end,
+    -- on_init = function(client, _)
+    --     client.notify('workspace/didChangeConfiguration', { settings = client.config.settings })
+    -- end,
     on_attach = function(client, bufnr)
       require('config.mappings').setup_lsp(bufnr)
     end,
   }
 end
 
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "java",
+--   callback = function()
+--     local jdtls_config = config()
+--     local capabilities = vim.lsp.protocol.make_client_capabilities()
+--     capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+--     jdtls_config.capabilities = capabilities
+--     -- require("jdtls").setup_dap({})
+--     -- print("start jdtls: " .. os.date())
+--     require("jdtls").start_or_attach(jdtls_config)
+--   end,
+--   group = vim.api.nvim_create_augroup("jdtls_setup", { clear = true }),
+-- })
+
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "java",
+--   callback = function()
+--     -- Only start if no JDTLS client exists for this root
+--     local root_dir = require('jdtls.setup').find_root({ '.git' })
+--     local clients = vim.lsp.get_active_clients({ name = "jdtls" })
+--
+--     for _, client in ipairs(clients) do
+--       if client.config.root_dir == root_dir then
+--         print("JDTLS already running for workspace:", root_dir)
+--         return
+--       end
+--     end
+--
+--     print("Starting JDTLS for workspace:", root_dir)
+--     local jdtls_config = config()
+--     local capabilities = vim.lsp.protocol.make_client_capabilities()
+--     capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+--     jdtls_config.capabilities = capabilities
+--     require("jdtls").start_or_attach(jdtls_config)
+--   end,
+--   group = vim.api.nvim_create_augroup("jdtls_setup", { clear = true }),
+-- })
+
+-- Simple autocmd that lets nvim-jdtls handle everything
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "java",
+--   callback = function()
+--     local jdtls_config = config()
+--     local capabilities = vim.lsp.protocol.make_client_capabilities()
+--     capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+--     jdtls_config.capabilities = capabilities
+--     require("jdtls").start_or_attach(jdtls_config)
+--   end,
+--   group = vim.api.nvim_create_augroup("jdtls_setup", { clear = true }),
+-- })
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "java",
   callback = function()
+    -- Only start if no JDTLS client exists for this root
+    local root_dir = require('jdtls.setup').find_root({ '.git' })
+    local clients = vim.lsp.get_active_clients({ name = "jdtls" })
+
+    for _, client in ipairs(clients) do
+      if client.config.root_dir == root_dir then
+        print("JDTLS already running for workspace:", root_dir)
+        return
+      end
+    end
+
+    print("Starting JDTLS for workspace:", root_dir)
     local jdtls_config = config()
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
     jdtls_config.capabilities = capabilities
     require("jdtls").start_or_attach(jdtls_config)
   end,
+  group = vim.api.nvim_create_augroup("jdtls_setup", { clear = true }),
 })
 
+-- return {}
 return {
   "mfussenegger/nvim-jdtls",
-  ft = "java",
+  ft = { 'java' },
+  -- ft = "java",
+  jdtls_config = config
 }
--- dotfiles/.config/nvim/lua/plugins/lspconfig.lua
+++ dotfiles/.config/nvim/lua/plugins/lspconfig.lua
@@ -26,12 +26,20 @@ return {
       handlers = {
         -- Default handler for all installed servers
         function(server_name)
+          print("register handler for server_name: " .. server_name) 
           if server_name == 'jdtls' then
             -- These require specialized setup
             return
           end
+          if server_name == 'java-debug-adapter' then
+            return
+          end
+          if server_name == 'java-test' then
+            return
+          end
           require('lspconfig')[server_name].setup({
             on_attach = function(client, bufnr)
+              print("attach :" .. server_name)
               require('config.mappings').setup_lsp(bufnr)
             end,
             flags = {
@@ -39,9 +47,21 @@ return {
             }
           })
         end,
-      }
+      },
+      automatic_enable = {
+        exclude = { 'jdtls' }
+      },
     })
 
+    -- vim.api.nvim_create_autocmd('FileType', {
+    --     group = vim.api.nvim_create_augroup('lsp_define_java', { clear = true }),
+    --     pattern = 'java',
+    --     callback = function()
+    --         print("Start jdtls")
+    --         require('jdtls').start_or_attach(require('plugins.jdtls').jdtls_config())
+    --     end
+    -- })
+    --
     -- Auto format on save
     vim.api.nvim_create_autocmd({"BufWritePre"}, {
       pattern = {"*.tf", "*.tfvars"},
