a8b10c3ed25797ce7c862db75bc6414aa46b5a2f
Pieter van der Heijden <pieter.vanderheijden@cm.com>
2025-06-02 17:12:54 +0200

    nvim: refactor mappings.lua

--- dotfiles/.config/nvim/lua/config/mappings.lua
+++ dotfiles/.config/nvim/lua/config/mappings.lua
@@ -34,66 +34,56 @@ function This.setup_lsp(bufnr)
     vim.keymap.set("n", rhs, lhs, opts)
   end
 
-
   -- LSP
-  -- Mappings.
-  local function opts(desc)
-    return { desc = 'LSP: ' .. desc, noremap = true, silent = true }
-  end
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  nnoremap('gD', vim.lsp.buf.declaration, 'Go to declaration')
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts('Go to definition'))
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts('Go to implementation'))
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts('Hover text'))
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts('Show signature'))
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>',
-    opts('Add workspace folder'))
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>',
-    opts('Remove workspace folder'))
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl',
-    '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts('List workspace folders'))
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>',
-    opts('Go to type definition'))
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts('Rename'))
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts('Find refenerences'))
-  nnoremap('<space>ca', vim.lsp.buf.code_action, "Code actions")
-  vim.keymap.set('v', "<space>ca", "<ESC><CMD>lua vim.lsp.buf.range_code_action()<CR>",
-    { noremap = true, silent = true, buffer = bufnr, desc = "Code actions" })
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>',
-    opts("Go to previous diagnostic"))
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts("Go to next diagnostic"))
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>',
-    opts("Set loclist for diagnostic"))
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>',
-    opts('Format file'))
+  nnoremap('gD', vim.lsp.buf.declaration, 'LSP: Go to declaration')
+  nnoremap('gd', vim.lsp.buf.definition, 'LSP: Go to definition')
+  nnoremap('gi', vim.lsp.buf.implementation, 'LSP: Go to implementation')
+  nnoremap('K', vim.lsp.buf.hover, 'LSP: Hover text')
+  nnoremap('<C-k>', vim.lsp.buf.signature_help, 'LSP: Show signature')
+  nnoremap('<leader>wa', vim.lsp.buf.add_workspace_folder, 'LSP: Add workspace folder')
+  nnoremap('<leader>wr', vim.lsp.buf.remove_workspace_folder, 'LSP: Remove workspace folder')
+  nnoremap('<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
+    'LSP: List workspace folders')
+  nnoremap('<leader>D', vim.lsp.buf.type_definition, 'LSP: Go to type definition')
+  nnoremap('<leader>rn', vim.lsp.buf.rename, 'LSP: Rename')
+  nnoremap('gr', vim.lsp.buf.references, 'LSP: Find references')
+  nnoremap('<leader>ca', vim.lsp.buf.code_action, "LSP: Code actions")
+  vim.keymap.set('v', "<leader>ca", function() vim.lsp.buf.range_code_action() end,
+    { noremap = true, silent = true, buffer = bufnr, desc = "LSP: Code actions" })
+  nnoremap('<leader>e', function() vim.diagnostic.open_float(nil, { focusable = false }) end,
+    'LSP: Open diagnostic float')
+  nnoremap('[d', function() vim.diagnostic.jump({ count = -1 }) end, 'LSP: Go to previous diagnostic')
+  nnoremap(']d', function() vim.diagnostic.jump({ count = 1 }) end, 'LSP: Go to next diagnostic')
+  nnoremap('<leader>q', function() vim.diagnostic.setqflist() end, 'LSP: Set quickfix for diagnostic')
+  nnoremap('<leader>f', function() vim.lsp.buf.format({ async = true }) end, 'LSP: Format file')
 
   -- Reload diagnostics
   -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rd', '<cmd>lua vim.diagnostic.reset() vim.diagnostic.show()<CR>', opts)
 
   -- DAP
-  nnoremap("<space>bb", "<cmd>lua require('dap').toggle_breakpoint()<cr>", "Set breakpoint")
-  nnoremap("<leader>bc", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>",
-    "Set conditional breakpoint")
-  nnoremap("<leader>bl", "<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<cr>",
-    "Set log point")
-  nnoremap('<leader>br', "<cmd>lua require'dap'.clear_breakpoints()<cr>", "Clear breakpoints")
-  nnoremap('<leader>ba', '<cmd>Telescope dap list_breakpoints<cr>', "List breakpoints")
+  local dap = require('dap')
+  nnoremap("<leader>bb", dap.toggle_breakpoint, "DAP: Set breakpoint")
+  nnoremap("<leader>bc", function() dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end,
+    "DAP: Set conditional breakpoint")
+  nnoremap("<leader>bl", function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end,
+    "DAP: Set log point")
+  nnoremap('<leader>br', dap.clear_breakpoints, "DAP: Clear breakpoints")
+  nnoremap('<leader>ba', '<cmd>Telescope dap list_breakpoints<cr>', "DAP: List breakpoints")
 
-  nnoremap("<leader>dc", "<cmd>lua require'dap'.continue()<cr>", "Continue")
-  nnoremap("<leader>dj", "<cmd>lua require'dap'.step_over()<cr>", "Step over")
-  nnoremap("<leader>dk", "<cmd>lua require'dap'.step_into()<cr>", "Step into")
-  nnoremap("<leader>do", "<cmd>lua require'dap'.step_out()<cr>", "Step out")
-  nnoremap('<leader>dd', "<cmd>lua require'dap'.disconnect()<cr>", "Disconnect")
-  nnoremap('<leader>dt', "<cmd>lua require'dap'.terminate()<cr>", "Terminate")
-  nnoremap("<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>", "Open REPL")
-  nnoremap("<leader>dl", "<cmd>lua require'dap'.run_last()<cr>", "Run last")
-  nnoremap('<leader>di', function() require "dap.ui.widgets".hover() end, "Variables")
+  nnoremap("<leader>dc", dap.continue, "DAP: Continue")
+  nnoremap("<leader>dj", dap.step_over, "DAP: Step over")
+  nnoremap("<leader>dk", dap.step_into, "DAP: Step into")
+  nnoremap("<leader>do", dap.step_out, "DAP: Step out")
+  nnoremap('<leader>dd', dap.disconnect, "DAP: Disconnect")
+  nnoremap('<leader>dt', dap.terminate, "DAP: Terminate")
+  nnoremap("<leader>dr", dap.repl.toggle, "DAP: Open REPL")
+  nnoremap("<leader>dl", dap.run_last, "DAP: Run last")
+  nnoremap('<leader>di', function() require "dap.ui.widgets".hover() end, "DAP: Variables")
   nnoremap('<leader>d?', function()
     local widgets = require "dap.ui.widgets"; widgets.centered_float(widgets.scopes)
-  end, "Scopes")
-  nnoremap('<leader>df', '<cmd>Telescope dap frames<cr>', "List frames")
-  nnoremap('<leader>dh', '<cmd>Telescope dap commands<cr>', "List commands")
+  end, "DAP: Scopes")
+  nnoremap('<leader>df', '<cmd>Telescope dap frames<cr>', "DAP: List frames")
+  nnoremap('<leader>dh', '<cmd>Telescope dap commands<cr>', "DAP: List commands")
 end
 
 return This
