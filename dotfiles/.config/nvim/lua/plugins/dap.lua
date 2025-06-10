0f5e90656c268a50eb8baeb277276ac6f6995b49
Pieter van der Heijden <pieter.vanderheijden@cm.com>
2025-06-10 06:44:51 +0200

    nvim: customize nvim-dap-ui to highlight line when breakpoint is hit

--- dotfiles/.config/nvim/lua/plugins/dap.lua
+++ dotfiles/.config/nvim/lua/plugins/dap.lua
@@ -4,9 +4,6 @@ return {
     'mfussenegger/nvim-dap',
     config = function()
       local dap = require("dap")
-
-      vim.fn.sign_define('DapBreakpoint', { text = '🔴', texthl = '', linehl = '', numhl = '' })
-      vim.fn.sign_define('DapStopped', { text = '▶️', texthl = 'blue', linehl = 'green', numhl = '' })
     end
   },
 
@@ -18,7 +15,108 @@ return {
       local dap = require("dap")
       local dapui = require("dapui")
 
-      dapui.setup()
+      dapui.setup({
+        icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
+        mappings = {
+          -- Use a table to apply multiple mappings
+          expand = { "<CR>", "<2-LeftMouse>" },
+          open = "o",
+          remove = "d",
+          edit = "e",
+          repl = "r",
+          toggle = "t",
+        },
+        -- Expand lines larger than the window
+        expand_lines = vim.fn.has("nvim-0.7") == 1,
+        -- Layouts define sections of the screen to place windows.
+        layouts = {
+          {
+            elements = {
+              -- Elements can be strings or table with id and size keys.
+              { id = "scopes", size = 0.25 },
+              "breakpoints",
+              "stacks",
+              "watches",
+            },
+            size = 40, -- 40 columns
+            position = "left",
+          },
+          {
+            elements = {
+              "repl",
+              "console",
+            },
+            size = 0.25, -- 25% of total lines
+            position = "bottom",
+          },
+        },
+        controls = {
+          -- Requires Neovim nightly (or 0.8 when released)
+          enabled = true,
+          -- Display controls in this element
+          element = "repl",
+          icons = {
+            -- pause = "",
+            -- play = "",
+            -- step_into = "",
+            -- step_over = "",
+            -- step_out = "",
+            -- step_back = "",
+            -- run_last = "↻",
+            -- terminate = "□",
+          },
+        },
+        floating = {
+          max_height = nil,  -- These can be integers or a float between 0 and 1.
+          max_width = nil,   -- Floats will be treated as percentage of your screen.
+          border = "single", -- Border style. Can be "single", "double" or "rounded"
+          mappings = {
+            close = { "q", "<Esc>" },
+          },
+        },
+        windows = { indent = 1 },
+        render = {
+          max_type_length = nil, -- Can be integer or nil.
+          max_value_lines = 100, -- Can be integer or nil.
+        }
+      })
+
+
+      -- Set up DAP signs and highlights
+      vim.fn.sign_define('DapBreakpoint', {
+        text = '🔴',
+        texthl = 'DapBreakpoint',
+        linehl = '',
+        numhl = 'DapBreakpoint'
+      })
+
+      vim.fn.sign_define('DapStopped', {
+        text = '▶',
+        texthl = 'DapStopped',
+        linehl = 'DapStoppedLine',
+        numhl = 'DapStopped'
+      })
+
+      vim.fn.sign_define('DapBreakpointCondition', {
+        text = '🔶',
+        texthl = 'DapBreakpointCondition',
+        linehl = '',
+        numhl = ''
+      })
+
+      vim.fn.sign_define('DapBreakpointRejected', {
+        text = '❌',
+        texthl = 'DapBreakpointRejected',
+        linehl = '',
+        numhl = ''
+      })
+
+      -- Define highlight groups
+      vim.api.nvim_set_hl(0, 'DapStoppedLine', { default = true, link = 'Visual' })
+      vim.api.nvim_set_hl(0, 'DapBreakpoint', { fg = '#e06c75' })
+      vim.api.nvim_set_hl(0, 'DapStopped', { fg = '#98c379' })
+      vim.api.nvim_set_hl(0, 'DapBreakpointCondition', { fg = '#ffc777' })
+      vim.api.nvim_set_hl(0, 'DapBreakpointRejected', { fg = '#f87171' })
 
       -- Auto open/close UI when debugging starts/stops
       dap.listeners.before.attach.dapui_config = function()
