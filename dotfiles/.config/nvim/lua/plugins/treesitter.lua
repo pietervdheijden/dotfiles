f7688a54f6b564052ef46c7019e6e69322eaf8a8
Pieter van der Heijden <pietervdheijden@gmail.com>
2025-06-14 20:59:32 +0200

    nvim: install bash for treesitter

--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html", "java", "python", "terraform" },
+      ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html", "java", "python", "terraform", "bash" },
       auto_install = true,
       sync_install = false,
       highlight = { enable = true },
