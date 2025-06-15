102799bb298fa5a9d553b0b1d97384b32299e201
Pieter van der Heijden <pietervdheijden@gmail.com>
2025-06-15 10:07:44 +0200

    nvim: restore nvim-tree after restoring session

--- dotfiles/.config/nvim/lua/plugins/auto-session.lua
+++ dotfiles/.config/nvim/lua/plugins/auto-session.lua
@@ -1,11 +1,20 @@
 return {
   'rmagatti/auto-session',
   lazy = false,
+  config = function()
+    require("auto-session").setup({
+      auto_restore_enabled = true,
+      auto_save_enabled = true,
+      auto_session_suppress_dirs = { "~/", "/" },
+      post_restore_cmds = {
+        function()
+          -- Reopen nivm-tree after restoring session
+          require('nvim-tree.api').tree.open()
 
-  ---enables autocomplete for opts
-  ---@module "auto-session"
-  opts = {
-    suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
-    -- log_level = 'debug',
-  }
+          -- Return focus to buffer
+          vim.cmd.wincmd('p')
+        end
+      },
+    })
+  end
 }
