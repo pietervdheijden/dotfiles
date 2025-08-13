a239c3310a1dd4ce15a924e455c499b77b5bb1ef
Pieter van der Heijden <pieter.vanderheijden@cm.com>
2025-08-13 11:03:28 +0200

    nvim: add java mapping "tl" to re-run the last test method or class

--- dotfiles/.config/nvim/after/ftplugin/java.lua
+++ dotfiles/.config/nvim/after/ftplugin/java.lua
@@ -15,5 +15,47 @@ map('n', '<leader>ec', jdtls.extract_constant, { desc = "JDTLS: extract constant
 map('n', '<leader>em', jdtls.extract_method, { desc = "JDTLS: extract method" })
 
 -- Test mapping
-map('n', '<leader>tc', jdtls.test_class, { desc = "JDTLS: test class" })
-map('n', '<leader>tm', jdtls.test_nearest_method, { desc = "JDTLS: test nearest method" })
+vim.keymap.set('n', '<leader>tm', function()
+  vim.g.jdtls_last_test = {
+    file = vim.fn.expand('%:p'),
+    line = vim.fn.line('.'),
+    col = vim.fn.col('.'),
+    type = 'method'
+  }
+  require('jdtls').test_nearest_method()
+end, { desc = "JDTLS: test nearest method" })
+
+vim.keymap.set('n', '<leader>tc', function()
+  vim.g.jdtls_last_test = {
+    file = vim.fn.expand('%:p'),
+    line = vim.fn.line('.'),
+    col = vim.fn.col('.'),
+    type = 'class'
+  }
+  require('jdtls').test_class()
+end, { desc = "JDTLS: test class" })
+
+vim.keymap.set('n', '<leader>tl', function()
+  if vim.g.jdtls_last_test then
+    -- Store current location
+    local current_buf = vim.api.nvim_get_current_buf()
+    local current_pos = vim.api.nvim_win_get_cursor(0)
+
+    -- Temporarily switch to test file
+    vim.cmd('edit ' .. vim.g.jdtls_last_test.file)
+    vim.fn.cursor(vim.g.jdtls_last_test.line, vim.g.jdtls_last_test.col)
+
+    -- Run the test
+    if vim.g.jdtls_last_test.type == 'method' then
+      require('jdtls').test_nearest_method()
+    elseif vim.g.jdtls_last_test.type == 'class' then
+      require('jdtls').test_class()
+    end
+
+    -- Switch back to original location
+    vim.api.nvim_set_current_buf(current_buf)
+    vim.api.nvim_win_set_cursor(0, current_pos)
+  else
+    vim.notify("No previous test to run", vim.log.levels.WARN)
+  end
+end, { desc = "JDTLS: run last test" })
