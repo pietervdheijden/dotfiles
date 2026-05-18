775e3f30cd8fc69b2671f3437184442a20cb674b
Pieter van der Heijden <pietervdheijden@gmail.com>
2026-05-18 06:38:04 +0200

    zsh: make kubecontext segment readable on catppuccin

--- dotfiles/.config/zsh/.p10k.zsh
+++ dotfiles/.config/zsh/.p10k.zsh
@@ -1409,8 +1409,8 @@
       # '*prod*'  PROD    # These values are examples that are unlikely
       # '*test*'  TEST    # to match your needs. Customize them as needed.
       '*'       DEFAULT)
-  typeset -g POWERLEVEL9K_KUBECONTEXT_DEFAULT_FOREGROUND=7
-  typeset -g POWERLEVEL9K_KUBECONTEXT_DEFAULT_BACKGROUND=5
+  typeset -g POWERLEVEL9K_KUBECONTEXT_DEFAULT_FOREGROUND=254
+  typeset -g POWERLEVEL9K_KUBECONTEXT_DEFAULT_BACKGROUND=24
   # typeset -g POWERLEVEL9K_KUBECONTEXT_DEFAULT_VISUAL_IDENTIFIER_EXPANSION='⭐'
 
   # Use POWERLEVEL9K_KUBECONTEXT_CONTENT_EXPANSION to specify the content displayed by kubecontext
